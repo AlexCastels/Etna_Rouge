cdb7c22b013c4988665a7db154b49cb00b8a8ef4
Dennis <emanuele.glorioso@gmail.com>
2024-05-07 15:44:51 +0200

    TLGPE-07:ADD: Carousel and Carousel hook

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
-import { Plp } from './components/Plp'
+// import { Plp } from './components/Plp'
+import { Carousel } from './components/Carousel.tsx'
 
 function App() {
-    
+    const items: string[] = ['https://images.pexels.com/photos/13939465/pexels-photo-13939465.jpeg?auto=compress&cs=tinysrgb&w=600', 'Item 2', 'Item 3', 'Item 4','Item 5','Item 6','Item 7'];
+
     return (
         <>
-            <Plp/>
+            {/* <Plp/> */}
+            <Carousel items={items}/>
         </>
     )
 }
