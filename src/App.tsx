d308e49f31fe950d844f427cefed3f9f87b695e8
Alex <alessandrofrancescocastelli@outlook.it>
2024-05-14 15:07:49 +0200

    Aggiunto comp Paypal + modifiche stile varie

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,6 @@
 import Cart from "./components/Cart";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./style.scss";
-import { PayForm } from "./components/PayForm"
-import { SelectPayment } from './components/SelectPayment'
-import { CreditCardForm } from "./components/CreditCardForm";
 import "./components/PDP/Pdp.scss"
 // import { Plp } from './components/Plp';
 // import { PayForm } from "./components/PayForm"
@@ -12,6 +9,9 @@ import { Pdp } from "./components/PDP/Pdp.tsx";
 import ProductCard from "./components/ProductCard.tsx";
 import Paypal from "./components/payment/Paypal.tsx";
 import LandingPage from "./components/landingPage/LandingPage.tsx";
+import { SelectPayment } from "./components/payment/SelectPayment.tsx";
+import { PayForm } from "./components/payment/PayForm.tsx";
+import { CreditCardForm } from "./components/payment/CreditCardForm.tsx";
 
 
 
@@ -21,21 +21,21 @@ function App() {
     return (
         <>
             <BrowserRouter>
-                <LandingPage />
-                  {/*  <Routes>
+                {/* <LandingPage /> */}
+                {/*  <Routes>
                     <Route path="/" element={<LandingPage />} />
-                 <Route path="/plp/productId" element={<Pdp {...obj} />} />
+                    <Route path="/plp/productId" element={<Pdp {...obj} />} />
                     <Route path="/pdp" element={<ProductCard />} />
                     <Route path="/Cart" element={<Cart />} />
                     <Route path="/SelectPayment" element={<SelectPayment />} />
                     <Route path="/DeliveryForm" element={<PayForm/>} /> */}
             
         
-                {/* <Routes> route per Chekout 
+                <Routes>
                     <Route path='/SelectPayment' element={<SelectPayment/>}/>
                     <Route path='/DeliveryForm' element={<PayForm/>}/>
                     <Route path='/CreditCardForm' element={<CreditCardForm/>}/>
-                </Routes> */}
+                </Routes>
             </BrowserRouter>       
         </>
     );
