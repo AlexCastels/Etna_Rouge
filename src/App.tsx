d011625859c6c7e4887653e7424b13276d7c3986
Alex <alessandrofrancescocastelli@outlook.it>
2024-05-14 12:08:43 +0200

    creato componente CreditCardForm

--- src/App.tsx
+++ src/App.tsx
@@ -3,27 +3,22 @@ import Cart from "./components/Cart"
 import { ProductCard } from "./components/Plp"
 import {BrowserRouter,Route,Routes} from 'react-router-dom'
 import "./style.scss";
-import { Plp } from './components/Plp'
 import { PayForm } from "./components/PayForm"
 import { SelectPayment } from './components/SelectPayment'
 import { CreditCardForm } from "./components/CreditCardForm";
 
-
-
-
 function App() {
-
     return (
         <>
-        {/* <BrowserRouter>
-            <Routes>
-                <Route path="/" element={<ProductCard/>}/>
-                <Route path="/Cart" element={<Cart/>}/>
-                <Route path='/SelectPayment' element={<SelectPayment/>}/>
-                <Route path='/DeliveryForm' element={<PayForm/>}/>
-            </Routes>
-        </BrowserRouter> */}
-        <CreditCardForm/>
+            <BrowserRouter>
+                <Routes>
+                    <Route path="/" element={<ProductCard/>}/>
+                    <Route path="/Cart" element={<Cart/>}/>
+                    <Route path='/SelectPayment' element={<SelectPayment/>}/>
+                    <Route path='/DeliveryForm' element={<PayForm/>}/>
+                    <Route path='/CreditCardForm' element={<CreditCardForm/>}/>
+                </Routes>
+            </BrowserRouter>       
         </>
     )
 }
--- src/components/CreditCardForm.tsx
+++ src/components/CreditCardForm.tsx
@@ -1,31 +1,64 @@
-import Button from './UI/button/Button';
 import './creditCardForm.scss'
 import { useState } from "react"
-// https://codesandbox.io/p/sandbox/thirsty-heisenberg-fwgtb?file=%2Fsrc%2FCreditCard.js%3A18%2C54
-export function CreditCardForm(){
+import { useNavigate } from 'react-router-dom';
 
+export function CreditCardForm(){
+    
+    interface CreditDate {
+        month : number ,
+        year : number,
+    }
     interface CreditCard{
-        creditNumber: number;
-        creditName: string;
-        creditDate: number;
-        cvv: number;
+        creditName: string | null;
+        creditNumber: string | null;
+        creditDate: CreditDate;
+        cvv: number | null;
+    }
+    
+    const navigate = useNavigate()
+    const [creditName , setCreditName] = useState<string | null>('')
+    const [creditNumber , setCreditNumber] = useState<string | null>('')
+    const [creditDate , setCreditDate] = useState<CreditDate>({
+        month : 0,
+        year : 0,
+    })
+    const [cvv , setCvv] = useState<number | null>(null)
+
+    const creditObj:CreditCard = {
+        creditName : creditName,
+        creditNumber : creditNumber,
+        creditDate : creditDate,
+        cvv : cvv
     }
 
-    const [input , setInput] = useState<CreditCard>({
-        creditNumber: 0,
-        creditName: '',
-        creditDate: 0,
-        cvv: 0
-    })
+    function handleCreditName(e:React.FormEvent<HTMLInputElement>){
+        setCreditName(e.currentTarget.value)
+    }
 
-    function handleInput(e:React.ChangeEvent<HTMLInputElement>){
-        // const regex = /^[0-9]{3,4}$/
-        const name = e.target.name
-        const value = e.target.value
-        // if(name === 'cvv'){
-        //     handleRegex(e.target.value , regex)
-        // } 
-        setInput((prev) => {
+    function handleCreditNumber(e:React.ChangeEvent<HTMLInputElement>){
+        //rimuove eventuali spazi digitati
+        const inputVal = e.target.value.replace(/ /g, "");
+        //recupera solo caratteri digitati
+        let inputNumbersOnly = inputVal.replace(/\D/g, ""); 
+        if (inputNumbersOnly.length > 16) {
+            //utilizzo substr per poter prendere solamente i primi 16 caratteri digitati
+            //nel caso in cui ci fossero più di 16 caratteri digitati
+            inputNumbersOnly = inputNumbersOnly.substr(0, 16);
+        }
+        //con slip viene creato un 'array' composto da soli 4 caratteri
+        const splits = inputNumbersOnly.match(/.{1,4}/g);
+        let spacedNumber = "";
+        if (splits) {
+            //con join uniamo i 4 arr unendoli con un -
+            spacedNumber = splits.join("-"); 
+        }
+        setCreditNumber(spacedNumber);
+    }
+
+    function handleCreditDate(e:React.FormEvent<HTMLInputElement> | React.ChangeEvent<HTMLSelectElement>){
+        const value = e.currentTarget.value
+        const name = e.currentTarget.name
+        setCreditDate((prev) => {
             return {
                 ...prev,
                 [name] : value
@@ -33,13 +66,20 @@ export function CreditCardForm(){
         })
     }
 
-    // function handleRegex(value:any , regex:RegExp):boolean{
-    //     return regex.test(value)
-    // } onKeyDown={handleRegex} da mettere in input
+    function handleCvv(e:React.ChangeEvent<HTMLInputElement>){
+        const value = e.target.value.length        
+        if(value <= 3){
+            setCvv(Number(e.target.value))
+        }
+    }
+
+    function handleBtn(){
+        console.log(creditObj);
+        navigate('path componente')
+    }
 
     function handleForm(e:React.FormEvent){
-        e.preventDefault()
-        console.log(input);       
+        e.preventDefault()      
     }
 
     return (
@@ -48,15 +88,16 @@ export function CreditCardForm(){
                 <p>Enter Your Payment Credential</p>
             </div>
             <div className='creditForm-input-container'>
+                <img src="/assets/CreditFake.png" width={250} alt="Credi-fake-card" />
                     <div className='creditForm-input-img'>
-                        <input className='input-creditCard' required type="text" maxLength={16} placeholder='Credit Number *' name="creditNumber" onChange={handleInput} />
-                        <img className="credit-card-img" src='/public/assets/mastercard.png'></img>
+                        <input className='input-creditCard' required type="text" placeholder='Credit Number *' name="creditNumber" value={creditNumber} onChange={handleCreditNumber} />
+                        <img className="credit-card-img" src='/assets/mastercard.png'></img>
                     </div>
-                    <input className='input-creditCard' required type="text" name="creditName" placeholder='Credit Name *' onChange={handleInput}/>
+                    <input className='input-creditCard' required type="text" name="creditName" placeholder='Credit Name *' onChange={handleCreditName}/>
                     <div className='creditCard-select'>
-                        <input className='input-creditCard' disabled required type="text" maxLength={4} placeholder="Credit Date *" name='creditDate' onChange={handleInput}/>
+                        <input className='input-creditCard' disabled required type="text" maxLength={4} placeholder="Credit Date *" name='creditDate'/>
                         <div>
-                            <select name="month" className='creditCard-select-style' required>
+                            <select name="month" onChange={handleCreditDate} className='creditCard-select-style' required>
                                 <option value="MM">MM</option>
                                 <option value="gen">01</option>
                                 <option value="feb">02</option>
@@ -71,7 +112,7 @@ export function CreditCardForm(){
                                 <option value="nov">11</option>
                                 <option value="dic">12</option>
                             </select>
-                            <select name='year'className='creditCard-select-style' required>
+                            <select name='year'className='creditCard-select-style' onChange={handleCreditDate} required>
                                 <option value="YY">YY</option>
                                 <option value="23">23</option>
                                 <option value="24">24</option>
@@ -85,10 +126,9 @@ export function CreditCardForm(){
                             </select>    
                         </div>
                     </div>
-                <input className='input-creditCard' required type="text" placeholder='CVV *' name='cvv' maxLength={3}  onChange={handleInput} />    
+                <input className='input-creditCard' required type="number" placeholder='CVV *' name='cvv' value={cvv} onChange={handleCvv} />    
             </div>
-            <button>Invio</button>
-            {/* controllare come impostare pattern in react */}
+            <button disabled={!creditDate || !creditName || !cvv || !creditNumber} onClick={handleBtn}>Checkout</button>
         </form>
     )
 }
