3bcd7381e96fa1faec5fd5422cea58d0925aae5b
PaoloBaiada <paolosettemaggio@gmail.com>
2024-05-30 15:35:16 +0200

    rename scss

--- src/components/navbar/NavbarBottom.tsx
+++ src/components/navbar/NavbarBottom.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import "./navbarBottom.scss";
+import "./pippobarBottom.scss";
 import { toggleCart } from "../../redux/slices/cartSlice";
 import { useAppDispatch } from "../../redux/hook";
 import { useNavigate } from "react-router-dom";
