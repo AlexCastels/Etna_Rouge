4784e6edef8644234548a70504d9114c177fd942
Alessandro <alessandrofrancescocastelli@outlook.it>
2024-05-31 17:31:50 +0200

    Fixed logic ThankYouPage

--- src/components/payment/CreditCardForm.tsx
+++ src/components/payment/CreditCardForm.tsx
@@ -75,7 +75,7 @@ export function CreditCardForm() {
 
     function handleBtn() {
         console.log(creditObj);
-        navigate("/thankYouCard");
+        navigate("/thankYouDelivery" , {state : {delivery : false}});
     }
 
     function handleForm(e: React.FormEvent) {
