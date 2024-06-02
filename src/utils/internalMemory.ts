eed7352d11a280b9910ff13ffd503b7b080bd46e
Eleonora Melodia <138382771+EleonoraMelodia@users.noreply.github.com>
2024-06-02 17:25:12 +0200

    Update internalMemory.ts
    TLG:ER:SUP removed useless comments.

--- src/utils/internalMemory.ts
+++ src/utils/internalMemory.ts
@@ -1,15 +1,15 @@
 export const internalMemory = {
-  // metodo per ottenere i dati dal locale storage.
+
   get: (email: string): string | null => {
     const value = localStorage.getItem(email);
-    //se l'elemento esiste lo parsa in oggetto Javascript
+   
     return value ? JSON.parse(value) : null;
   },
-  //metodo per salvare i valori del locale storage e in questo caso trasforma in una stringa/in un Json gli elementi
+ 
   set: (email: string, value: string): void => {
     localStorage.setItem(email, JSON.stringify(value));
   },
-  // metoto per rimuovere gli elementi dal locale storage
+ 
   remove: (email: string): void => {
     localStorage.removeItem(email);
   },
